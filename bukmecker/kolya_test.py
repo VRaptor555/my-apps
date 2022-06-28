1dedce98492976130b39941e9ba4fa47b722fe74
BuildTools <unconfigured@null.spigotmc.org>
2022-06-28 22:34:02 +1100

    12345

--- bukmecker/kolya_test.py
+++ bukmecker/kolya_test.py
@@ -1,9 +1,7 @@
 import pandas as pd
-log = pd.read_csv("./bukmecker/data/log.csv", header = None)
+log = pd.read_csv("log.csv", header = None)
 log.columns = ['user_id','time','bet','win']
-log = log[~log.user_id.str.contains("error", na=False)]
-log['user_id'] = log.user_id.str.replace('Запись пользователя № - ', '')
+log = log.dropna(axis = 0)    
 log['time'] = log.time.str.replace('[', '')
-log.dropna(axis=0, subset=['user_id', 'time'])
 log['time'] = pd.to_datetime(log['time'])
-log['hour'] = log.time.dt.hour
+log['hour'] =  log.time.dt.hour
