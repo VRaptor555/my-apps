4772f2ea2e060d2cde2bd3683d17286e61d1f23e
VRaptor555 <puch399@yandex.ru>
2022-06-29 08:55:59 +1100

    123

---
+++ mes/pays_doc.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import zipfile as zip
+
+files = []
+# Загружаем реестры (или один реестр)
+
+def read_write_csv (files):
+    pays = []
+    ierror = 0
+    for ind, fil in enumerate(files):
+        pays.append(pd.read_csv('./mes/data/' + fil, header=None, sep=';', encoding='Windows-1251'))
+        if len(pays[ind].columns) < 13:
+            ierror = -1
+        pays[ind] = pays[ind].drop(8, axis=1)
+        pays[ind].to_csv('./mes/data/' + fil, header=None, sep=';', encoding='Windows-1251', index=False)
+    return ierror
+
+
+
+ires = read_write_csv(files)
+if ires == 0:
+    print('Удаление столбца прошло без ошибок!')
+else:
+    print('При удалении столбца произошла ошибка, возможно столбец уже удален!')
