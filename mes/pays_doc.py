0028e7cc87f24bb7e864d28fcbb826ae35ed9fcd
VRaptor555 <puch399@yandex.ru>
2022-07-21 08:12:24 +1100

    112

--- mes/pays_doc.py
+++ mes/pays_doc.py
@@ -5,22 +5,41 @@ import os
 catalog = 'data/'
 # Загружаем реестры (или один реестр)
 
-def read_write_csv (files):
-    pays = []
-    ierror = 0
-    for ind, fil in enumerate(files):
-        pays.append(pd.read_csv('data/' + fil, header=None, sep=';', encoding='Windows-1251'))
-        if len(pays[ind].columns) < 13:
-            ierror = -1
-        pays[ind] = pays[ind].drop(8, axis=1)
-        pays[ind].to_csv('data/' + fil, header=None, sep=';', encoding='Windows-1251', index=False)
-    return ierror
+def read_write_csv (t_dir, t_file):
+    pays = pd.read_csv(t_dir + t_file, header=None, sep=';', encoding='Windows-1251')
+    if len(pays.columns) < 13:
+        return -1
+    pays = pays.drop(8, axis=1)
+    pays.to_csv(t_dir + t_file, header=None, sep=';', encoding='Windows-1251', index=False)
+    return 'Удаление столбца прошло без ошибок!'
+
+def read_write_zip(z_dir, z_file):
+    zipFile = zip.ZipFile(z_dir + z_file, 'r')
+    if not (os.path.isdir(z_dir+'out/')):
+        os.mkdir(z_dir+'out/')    
+    zipFile_out = zip.ZipFile(z_dir+'out/'+z_file, 'w')
+    list_file = zipFile.namelist()
+    for txt_file in list_file:
+        zipFile.extract(txt_file)
+        if read_write_csv('', txt_file) == -1:
+            print('При удалении столбца произошла ошибка, возможно столбец уже удален!')
+            zipFile.close()
+            zipFile_out.close()
+            return -1
+        zipFile_out.write(txt_file)
+        os.remove(txt_file)
+    zipFile.close()
+    zipFile_out.close()
+    return 0
 
 files = os.listdir(catalog)
-reestry = list(filter(lambda x: x.endswith('.txt'), files))
+zip_reestr = list(filter(lambda x: x.endswith('.zip'), files))
+#reestry = list(filter(lambda x: x.endswith('.txt'), files))
 
-ires = read_write_csv(reestry)
-if ires == 0:
-    print('Удаление столбца прошло без ошибок!')
-else:
-    print('При удалении столбца произошла ошибка, возможно столбец уже удален!')
+if len(zip_reestr) > 0:
+    for zipf in zip_reestr:
+        print(read_write_zip(catalog, zipf))
+
+#if len(reestry) > 0:
+#    for txt_f in reestry:
+#        print(read_write_csv(catalog, reestry))
